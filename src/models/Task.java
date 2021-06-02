db9f3bff2cdc71793c0a75478397a2d1fe11ece6
seiichiro.mochizuki <mochizuki_s@digicomnet.co.jp>
2021-06-02 19:40:33 +0900

    Add all files but server is in error

--- src/models/Task.java
+++ src/models/Task.java
@@ -15,8 +15,12 @@ import javax.persistence.Table;
 @NamedQueries({
     @NamedQuery(
         name = "getAllTasks",
-        query = "SELECT t FROM Task  t ORDER BY t.id DESC"
-    )
+        query = "SELECT t FROM Task AS t ORDER BY t.id DESC"
+    ),
+    @NamedQuery(
+            name = "getTasksCount",
+            query = "SELECT COUNT(t) FROM Task AS t"
+        )
 })
 @Table(name = "tasks")
 public class Task {
